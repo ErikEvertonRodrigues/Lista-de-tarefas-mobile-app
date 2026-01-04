f5dbaf03d7a35c1a4c33a1d53168ebcc7c856658
Erik Rodrigues <erik.webprogrammer@gmail.com>
2026-01-03 23:31:08 -0300

    add createTask function

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Alert } from 'react-native';
 
 export default function App() {
+
+  const [inputText, SetInputText] = useState('Texto padrão');
 
   type Task = {
     id: number
@@ -9,15 +12,39 @@ export default function App() {
     done: boolean
   }
 
-  let tasks: Task[];
+  let tasks: Task[] = [];
+
+  function createTask(id: number, taskText: string) {
+    const newTask: Task = {
+      id: id,
+      text: taskText,
+      done: false,
+    }
+
+    return newTask;
+  }
+
+  function addTask() {
+    
+  }
 
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <Text style={styles.inputText}>Digite o nome da tarefa: </Text>
-        <TextInput
-          style={styles.input}
-        />
+      <View>
+        <View style={styles.inputContainer}>
+          <Text style={styles.inputText}>Digite o nome da tarefa: </Text>
+          <TextInput
+            style={styles.input}
+
+          />
+        </View>
+          <Button
+            title='Adicionar tarefa'
+            onPress={() => addTask()}
+          />
+      </View>
+      <View>
+        <Text style={styles.tasksTitle}>Tarefas adicionadas</Text>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -29,22 +56,29 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
 
   input: {
     borderWidth: 1,
-    borderRadius: 2,
-    width: 100,
+    borderRadius: 5,
+    width: 130,
   },
 
   inputContainer: {
     flex: 0,
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginBottom: 10,
   },
 
   inputText: {
-    
-  }
+    fontSize: 18,
+    fontWeight: '600',
+  },
+
+  tasksTitle: {
+    fontSize: 22,
+    fontWeight: 'bold',
+  },
 });
